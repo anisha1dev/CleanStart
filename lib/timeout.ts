85d605b45828e9da6831e840be19d21778584033
Anisha <anisha1dev@gmail.com>
2026-02-26 05:40:39 +0400

    Fix timeout typing for Supabase query builders

--- lib/timeout.ts
+++ lib/timeout.ts
@@ -1,9 +1,12 @@
-export async function withTimeout<T>(promise: Promise<T>, timeoutMs: number, label = 'Operation'): Promise<T> {
-  return Promise.race([
-    promise,
+export async function withTimeout<T>(
+  promise: PromiseLike<T> | Promise<T>,
+  timeoutMs: number,
+  label = 'Operation',
+): Promise<T> {
+  return Promise.race<T>([
+    Promise.resolve(promise),
     new Promise<T>((_, reject) => {
       setTimeout(() => reject(new Error(`${label} timed out after ${timeoutMs}ms`)), timeoutMs);
     }),
   ]);
 }
-
